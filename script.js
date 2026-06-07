764698c19d065950bf946986797a35a3850d3c8a
getkrowd-del <getkrowd@gmail.com>
2026-06-07 07:41:41 -0400

    Deploy landing page: Beef & Bird - Utica NY

---
+++ script.js
@@ -0,0 +1,19 @@
+tailwind.config = {
+            theme: {
+                extend: {
+                    colors: {
+                        brandTan: '#D2B48C',
+                        brandTanLight: '#F3EFE9',
+                        brandBrown: '#221915',
+                        brandPlum: '#612B58',
+                        brandPlumLight: '#7E3E74', 
+                        brandRed: '#C1272D',
+                        brandRedLight: '#DC353C',
+                    },
+                    fontFamily: {
+                        serif: ['Playfair Display', 'serif'],
+                        sans: ['Plus Jakarta Sans', 'sans-serif'],
+                    }
+                }
+            }
+        }
